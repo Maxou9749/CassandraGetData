db4d29b85c23ae46f96fd8c966d2e17da4d90932
Maxou9749 <max.974@hotmail.fr>
2018-02-13 11:34:09 +0400

    Ajout de donnees temporelle dans la liste de capteurs

--- Recuperation_donnees_capteur_sup_base_de_temps_5_ans.R
+++ Recuperation_donnees_capteur_sup_base_de_temps_5_ans.R
@@ -7,3 +7,16 @@
 # Chargement de la library rLE2P
 library(rLE2P)
 
+Liste <- read.table("C:/Users/max97/Documents/Espace de travail Git/CassandraGetData/Liste_capteurs_base_de_temps_sup_5_ans.txt", sep = "\t")
+
+# Selection des capteurs solaires pour chaque transaction
+Ray_D <- c("FD_Avg", "FDnum_Avg")
+Ray_G <- c("FG_Avg", "FGnum_Avg")
+
+Saint_Leu <- "1465986548"
+Saint_Andre <- "1465986253"
+Saint_Pierre <- "1465989663"
+Moufia_BDN <- "1465986423"
+La_Possession <- "1465986484"
+
+CassandraGetData(idTransaction = Saint_Leu, idSensor = Ray_D[1], timeStart = )
