ce7f6c6befdde261131709e3d4f05333cc5b1f79
Maxou9749 <max.974@hotmail.fr>
2018-02-13 15:56:26 +0400

    Avancée du programme de récupération de données

--- Recuperation_donnees_capteur_sup_base_de_temps_5_ans.R
+++ Recuperation_donnees_capteur_sup_base_de_temps_5_ans.R
@@ -19,4 +19,30 @@ Saint_Pierre <- "1465989663"
 Moufia_BDN <- "1465986423"
 La_Possession <- "1465986484"
 
-CassandraGetData(idTransaction = Saint_Leu, idSensor = Ray_D[1], timeStart = )
+Liste_tr <- c(Saint_Leu, Saint_Andre, Saint_Pierre, Moufia_BDN, La_Possession)
+
+# Rayonnement Diffus
+Diffus_1 <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_D[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Diffus_2 <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_D[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Diffus_3 <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_D[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Diffus_4 <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_D[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Diffus_5 <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_D[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+
+# Rayonnement Global
+# Recuperation du mois de novembre
+Global_1 <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_G[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Global_2 <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_G[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Global_3 <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_G[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Global_4 <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_G[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+Global_5 <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_G[1], timeStart = "2012-11-01 00:01:00", timeEnd = "2012-12-01 00:00:00")
+
+
+temps <- Diffus_1$date
+temps <- sub(pattern="H", replacement=":",temps, fixed=TRUE)
+temps <- strptime(temps, format="%Y-%m-%d %H:%M:%S")
+
+plot(temps, Global_1$value, type = "l")
+lines(temps, Diffus_1$value, type = "l", col = 2)
+
+length(temps)
+
