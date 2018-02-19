3c8c0448329f6260a46a2c0a444c6dea45cc0fa8
Maxou9749 <max.974@hotmail.fr>
2018-02-19 17:01:12 +0400

    Mise à jour 19/02 17:01

--- test_recup_donnees_par_jour_sur_l_annee.R
+++ test_recup_donnees_par_jour_sur_l_annee.R
@@ -1,111 +1,95 @@
-# Decembre 2012
-dir.create("Decembre 2012")
+jr_mois <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+mois <- c("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre")
+annee <- c("2012", "2013", "2014", "2015", "2016", "2017")
 
-# Global
-G_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_G, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(G_SL, "C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Leu.txt", sep=";")
-G_SL <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Leu.txt", sep=";")
-G_SL <- na.omit(object = G_SL)
-Moy_Dec_G_SL <- mean(G_SL$value)
-G_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_G, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(G_SA, "C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Andre.txt", sep=";")
-G_SA <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Andre.txt", sep=";")
-G_SA <- na.omit(object = G_SA)
-Moy_Dec_G_SA <- mean(G_SA$value)
-G_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_G, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(G_SP, "C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Pierre.txt", sep=";")
-G_SP <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Leu.txt", sep=";")
-G_SP <- na.omit(object = G_SP)
-Moy_Dec_G_SP <- mean(G_SP$value)
-G_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_G, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(G_MBDN, "C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Moufia_BDN.txt", sep=";")
-G_MBDN <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Leu.txt", sep=";")
-G_MBDN <- na.omit(object = G_MBDN)
-Moy_Dec_G_MBDN <- mean(G_MBDN$value)
-G_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_G, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(G_LP, "C:/Users/max97/Documents/Decembre 2012/dec2012_Global_La_Possession.txt", sep=";")
-G_LP <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Global_Saint_Leu.txt", sep=";")
-G_LP <- na.omit(object = G_LP)
-Moy_Dec_G_LP <- mean(G_LP$value)
+# Création des dossiers
+setwd(dir = "~")
+dir.create("Base de données")
+setwd(dir = "Base de données")
+dir.create("2012")
+dir.create("2013")
+dir.create("2014")
+dir.create("2015")
+dir.create("2016")
+dir.create("2017")
 
-# Diffus
-D_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_D, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-dir.create("Decembre 2012")
-write.table(D_SL, "C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Leu.txt", sep=";")
-D_SL <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Leu.txt", sep=";")
-D_SL <- na.omit(object = D_SL)
-Moy_Dec_D_SL <- mean(D_SL$value)
-D_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_D, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(D_SA, "C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Andre.txt", sep=";")
-D_SA <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Andre.txt", sep=";")
-D_SA <- na.omit(object = D_SA)
-Moy_Dec_D_SA <- mean(D_SA$value)
-D_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_D, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(D_SP, "C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Pierre.txt", sep=";")
-D_SP <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Leu.txt", sep=";")
-D_SP <- na.omit(object = D_SP)
-Moy_Dec_D_SP <- mean(D_SP$value)
-D_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_D, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(D_MBDN, "C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Moufia_BDN.txt", sep=";")
-D_MBDN <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Leu.txt", sep=";")
-D_MBDN <- na.omit(object = D_MBDN)
-Moy_Dec_D_MBDN <- mean(D_MBDN$value)
-D_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_D, timeStart = "2012-12-01 00:01:00", timeEnd = "2012-12-31 23:59:00")
-write.table(D_LP, "C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_La_Possession.txt", sep=";")
-D_LP <- read.table("C:/Users/max97/Documents/Decembre 2012/dec2012_Diffus_Saint_Leu.txt", sep=";")
-D_LP <- na.omit(object = D_LP)
-Moy_Dec_D_LP <- mean(D_LP$value)
-
-
-for (an in seq(2013,2017)) {
-  # Janvier
-  for (jan in seq(1,31)) {
-    
-  }
-  # Fevrier
-  if (an == 2014) {for (fev in seq(1,29)) {}} else {for (fev in seq(1,28)) {}}
-  # Mars
-  for (mars in seq(1,31)) {
-    
-  }
-  # Avril
-  for (avr in seq(1,30)) {
-    
-  }
-  # Mai
-  for (mai in seq(1,31)) {
-    
-  }
-  # Juin
-  for (juin in seq(1,30)) {
-    
-  }
-  # Juillet
-  for (juill in seq(1,31)) {
-    
-  }
-  # Aout
-  for (aou in seq(1,31)) {
-    
-  }
-  # Septembre
-  for (sept in seq(1,30)) {
-    
-  }
-  # Octobre
-  for (oct in seq(1,31)) {
-    
-  }
-  # Novembre
-  for (nov in seq(1,30)) {
-    
-  }
-  # Decembre
-  for (dece in seq(1,31)) {
-    
+for (j in seq(1,6)) {
+  setwd(dir = "~/Base de données")
+  setwd(dir = annee[j])
+  for (i in seq(1,12)) {
+    dir.create(mois[i])
   }
 }
-# Janvier 2018
-for (jan in seq(1,31)) {
-  
+setwd(dir = "~/Base de données")
+
+# Recuperation des donnees et mise dans leur dossier respectifs
+for (j in seq(1,12)) {
+  date_debut <- c("2012-12-01 00:01:00", "2013-12-01 00:01:00", "2014-12-01 00:01:00", "2015-12-01 00:01:00", "2016-12-01 00:01:00", "2017-12-01 00:01:00")
+  date_fin <- c("2012-12-31 23:59:00", "2013-12-31 23:59:00", "2014-12-31 23:59:00", "2015-12-31 23:59:00", "2016-12-31 23:59:00", "2017-12-31 23:59:00")
+  for (i in seq(1,6)) {
+    # Global
+    setwd(dir = annee[i])
+    G_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_G, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(G_SL, "Global_Saint_Leu.txt", sep=";")
+    G_SL <- read.table("Global_Saint_Leu.txt", sep=";")
+    G_SL <- na.omit(object = G_SL)
+    write.table(G_SL, "Global_Saint_Leu.txt", sep=";")
+    
+    G_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_G, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(G_SA, "Global_Saint_Andre.txt", sep=";")
+    G_SA <- read.table("Global_Saint_Andre.txt", sep=";")
+    G_SA <- na.omit(object = G_SA)
+    write.table(G_SA, "Global_Saint_Andre.txt", sep=";")
+    
+    G_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_G, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(G_SP, "Global_Saint_Pierre.txt", sep=";")
+    G_SP <- read.table("Global_Saint_Leu.txt", sep=";")
+    G_SP <- na.omit(object = G_SP)
+    write.table(G_SP, "Global_Saint_Pierre.txt", sep=";")
+    
+    G_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_G, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(G_MBDN, "Global_Moufia_BDN.txt", sep=";")
+    G_MBDN <- read.table("Global_Saint_Leu.txt", sep=";")
+    G_MBDN <- na.omit(object = G_MBDN)
+    write.table(G_MBDN, "Global_Moufia_BDN.txt", sep=";")
+    
+    G_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_G, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(G_LP, "Global_La_Possession.txt", sep=";")
+    G_LP <- read.table("Global_Saint_Leu.txt", sep=";")
+    G_LP <- na.omit(object = G_LP)
+    write.table(G_LP, "Global_La_Possession.txt", sep=";")
+    
+    # Diffus
+    D_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_D, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(D_SL, "Diffus_Saint_Leu.txt", sep=";")
+    D_SL <- read.table("Diffus_Saint_Leu.txt", sep=";")
+    D_SL <- na.omit(object = D_SL)
+    write.table(D_SL, "Diffus_Saint_Leu.txt", sep=";")
+    
+    D_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_D, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(D_SA, "Diffus_Saint_Andre.txt", sep=";")
+    D_SA <- read.table("Diffus_Saint_Andre.txt", sep=";")
+    D_SA <- na.omit(object = D_SA)
+    write.table(D_SA, "Diffus_Saint_Andre.txt", sep=";")
+    
+    D_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_D, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(D_SP, "Diffus_Saint_Pierre.txt", sep=";")
+    D_SP <- read.table("Diffus_Saint_Leu.txt", sep=";")
+    D_SP <- na.omit(object = D_SP)
+    write.table(D_SP, "Diffus_Saint_Pierre.txt", sep=";")
+    
+    D_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_D, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(D_MBDN, "Diffus_Moufia_BDN.txt", sep=";")
+    D_MBDN <- read.table("Diffus_Saint_Leu.txt", sep=";")
+    D_MBDN <- na.omit(object = D_MBDN)
+    write.table(D_MBDN, "Diffus_Moufia_BDN.txt", sep=";")
+    
+    D_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_D, timeStart = date_debut[i], timeEnd = date_fin[i])
+    write.table(D_LP, "Diffus_La_Possession.txt", sep=";")
+    D_LP <- read.table("Diffus_Saint_Leu.txt", sep=";")
+    D_LP <- na.omit(object = D_LP)
+    write.table(D_LP, "Diffus_La_Possession.txt", sep=";")
+    
+    setwd(dir = "~/Base de données")
+  }
 }
