2da1948580f10d25084cd344967dc0f9f95e8dfe
Maxou9749 <max.974@hotmail.fr>
2018-03-29 11:39:20 +0400

    Modifications mineures

--- Recuperation_de_donnees_moufia_univ_test.R
+++ Recuperation_de_donnees_moufia_univ_test.R
@@ -9,23 +9,21 @@ library(rLE2P)
 
 
 # ---- Definition des variables utilisees ----
-Ray_D <- "FD_Avg"
-Ray_G <- "FG_Avg"
+Ray_G <- c("FG", "FG_Avg","GHI_014_Avg")
+Ray_D <- c("FD", "FD_Avg","DHI_014_Avg")
 
 Moufia_UR <- "1487856319"
 
-Liste_tr <- c(Moufia_UR)
-
 jr_mois <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 mois <- c("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre")
-annee <- c("2012", "2013", "2014", "2015", "2016", "2017","2018")
+annee <- c("2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017","2018")
 
 
 # ---- Création de la matrice de temps de debut et de fin ----
-time_d <- matrix(nrow = 12, ncol = 7)
-time_f <- matrix(nrow = 12, ncol = 7)
+time_d <- matrix(nrow = 12, ncol = 10)
+time_f <- matrix(nrow = 12, ncol = 10)
 
-for (j in seq(2012,2018)) {
+for (j in seq(2009,2018)) {
   for (i in seq(1,12)) {
     moi <- i
     an <- j
@@ -35,8 +33,8 @@ for (j in seq(2012,2018)) {
     jour <- jr_mois[i]
     date_f <- paste(an, moi, as.character(jour), sep = "-")
     date_f <- paste(date_f,"23:59:00", sep = " ")
-    time_d[i,j-2011] <- date_d
-    time_f[i,j-2011] <- date_f
+    time_d[i,j-2008] <- date_d
+    time_f[i,j-2008] <- date_f
   }
 }
 
@@ -45,6 +43,9 @@ for (j in seq(2012,2018)) {
 setwd(dir = "~")
 dir.create("Base de données")
 setwd(dir = "Base de données")
+dir.create("2009")
+dir.create("2010")
+dir.create("2011")
 dir.create("2012")
 dir.create("2013")
 dir.create("2014")
@@ -53,7 +54,7 @@ dir.create("2016")
 dir.create("2017")
 dir.create("2018")
 
-for (j in seq(2,6)) {
+for (j in seq(1,7)) {
   setwd(dir = "~/Base de données")
   setwd(dir = annee[j])
   for (i in seq(1,12)) {
@@ -61,83 +62,97 @@ for (j in seq(2,6)) {
   }
 }
 
-setwd(dir = "~/Base de données/2012")
-dir.create("Novembre")
-dir.create("Decembre")
+setwd(dir = "~/Base de données/2016")
+for (i in seq(1,9)) {
+  dir.create(mois[i])
+}
+setwd(dir = "~/Base de données/2017")
+for (i in seq(7,12)) {
+  dir.create(mois[i])
+}
 setwd(dir = "~/Base de données/2018")
-dir.create("Janvier")
+dir.create(mois[1])
 setwd(dir = "~/Base de données")
 
 
-# ---- Recuperation des donnees et mise dans leur dossier respectif ----
+# ---- Recuperation des donnees 2009 à 2012 ----
 for (j in seq(1,12)) {
-  for (i in seq(1,7)) {
+  for (i in seq(1,4)) {
     setwd(dir = "~/Base de données")
     setwd(dir = annee[i])
     # Si le mois n'existe pas, passer au mois suivant etc .
     if (dir.exists(mois[j]) == FALSE) {} else {
       setwd(dir = mois[j])
       # ---- Global ----
-      G_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_G, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(G_SL, "Global_Saint_Leu.txt", sep=";")
-      G_SL <- read.table("Global_Saint_Leu.txt", sep=";")
-      G_SL <- na.omit(object = G_SL)
-      write.table(G_SL, "Global_Saint_Leu.txt", sep=";")
       
-      G_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_G, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(G_SA, "Global_Saint_Andre.txt", sep=";")
-      G_SA <- read.table("Global_Saint_Andre.txt", sep=";")
-      G_SA <- na.omit(object = G_SA)
-      write.table(G_SA, "Global_Saint_Andre.txt", sep=";")
-      
-      G_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_G, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(G_SP, "Global_Saint_Pierre.txt", sep=";")
-      G_SP <- read.table("Global_Saint_Pierre.txt", sep=";")
-      G_SP <- na.omit(object = G_SP)
-      write.table(G_SP, "Global_Saint_Pierre.txt", sep=";")
-      
-      G_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_G, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(G_MBDN, "Global_Moufia_BDN.txt", sep=";")
-      G_MBDN <- read.table("Global_Moufia_BDN.txt", sep=";")
-      G_MBDN <- na.omit(object = G_MBDN)
-      write.table(G_MBDN, "Global_Moufia_BDN.txt", sep=";")
-      
-      G_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_G, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(G_LP, "Global_La_Possession.txt", sep=";")
-      G_LP <- read.table("Global_La_Possession.txt", sep=";")
-      G_LP <- na.omit(object = G_LP)
-      write.table(G_LP, "Global_La_Possession.txt", sep=";")
+      G_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_G[1], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
+      G_MUR <- read.table("Global_Moufia_UR.txt", sep=";")
+      G_MUR <- na.omit(object = G_MUR)
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
       
       # ---- Diffus ----
-      D_SL <- CassandraGetData(idTransaction = Liste_tr[1], idSensor = Ray_D, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(D_SL, "Diffus_Saint_Leu.txt", sep=";")
-      D_SL <- read.table("Diffus_Saint_Leu.txt", sep=";")
-      D_SL <- na.omit(object = D_SL)
-      write.table(D_SL, "Diffus_Saint_Leu.txt", sep=";")
+      D_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_D[1], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- read.table("Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- na.omit(object = D_MUR)
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
       
-      D_SA <- CassandraGetData(idTransaction = Liste_tr[2], idSensor = Ray_D, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(D_SA, "Diffus_Saint_Andre.txt", sep=";")
-      D_SA <- read.table("Diffus_Saint_Andre.txt", sep=";")
-      D_SA <- na.omit(object = D_SA)
-      write.table(D_SA, "Diffus_Saint_Andre.txt", sep=";")
+      setwd(dir = "~/Base de données")
       
-      D_SP <- CassandraGetData(idTransaction = Liste_tr[3], idSensor = Ray_D, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(D_SP, "Diffus_Saint_Pierre.txt", sep=";")
-      D_SP <- read.table("Diffus_Saint_Pierre.txt", sep=";")
-      D_SP <- na.omit(object = D_SP)
-      write.table(D_SP, "Diffus_Saint_Pierre.txt", sep=";")
+    }
+  }
+}
+# ---- Recuperation des donnees 2013 à 2016 ----
+for (j in seq(1,12)) {
+  for (i in seq(5,8)) {
+    setwd(dir = "~/Base de données")
+    setwd(dir = annee[i])
+    # Si le mois n'existe pas, passer au mois suivant etc .
+    if (dir.exists(mois[j]) == FALSE) {} else {
+      setwd(dir = mois[j])
+      # ---- Global ----
       
-      D_MBDN <- CassandraGetData(idTransaction = Liste_tr[4], idSensor = Ray_D, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(D_MBDN, "Diffus_Moufia_BDN.txt", sep=";")
-      D_MBDN <- read.table("Diffus_Moufia_BDN.txt", sep=";")
-      D_MBDN <- na.omit(object = D_MBDN)
-      write.table(D_MBDN, "Diffus_Moufia_BDN.txt", sep=";")
+      G_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_G[2], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
+      G_MUR <- read.table("Global_Moufia_UR.txt", sep=";")
+      G_MUR <- na.omit(object = G_MUR)
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
       
-      D_LP <- CassandraGetData(idTransaction = Liste_tr[5], idSensor = Ray_D, timeStart = time_d[j,i], timeEnd = time_f[j,i])
-      write.table(D_LP, "Diffus_La_Possession.txt", sep=";")
-      D_LP <- read.table("Diffus_La_Possession.txt", sep=";")
-      D_LP <- na.omit(object = D_LP)
-      write.table(D_LP, "Diffus_La_Possession.txt", sep=";")
+      # ---- Diffus ----
+      D_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_D[2], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- read.table("Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- na.omit(object = D_MUR)
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
+      
+      setwd(dir = "~/Base de données")
+      
+    }
+  }
+}
+# ---- Recuperation des donnees 2017 à 2018 ----
+for (j in seq(1,12)) {
+  for (i in seq(9,10)) {
+    setwd(dir = "~/Base de données")
+    setwd(dir = annee[i])
+    # Si le mois n'existe pas, passer au mois suivant etc .
+    if (dir.exists(mois[j]) == FALSE) {} else {
+      setwd(dir = mois[j])
+      # ---- Global ----
+      
+      G_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_G[3], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
+      G_MUR <- read.table("Global_Moufia_UR.txt", sep=";")
+      G_MUR <- na.omit(object = G_MUR)
+      write.table(G_MUR, "Global_Moufia_UR.txt", sep=";")
+      
+      # ---- Diffus ----
+      D_MUR <- CassandraGetData(idTransaction = Moufia_UR, idSensor = Ray_D[3], timeStart = time_d[j,i], timeEnd = time_f[j,i])
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- read.table("Diffus_Moufia_UR.txt", sep=";")
+      D_MUR <- na.omit(object = D_MUR)
+      write.table(D_MUR, "Diffus_Moufia_UR.txt", sep=";")
       
       setwd(dir = "~/Base de données")
       
